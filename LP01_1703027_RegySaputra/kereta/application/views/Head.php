a81ae9695178f7293e43eea451f773b9c035d0f4
Regy Saputra <regysaputra27@gmail.com>
2019-05-07 23:20:46 +0700

    Ganti title di dalam tag head menjadi Promnet

--- LP01_1703027_RegySaputra/kereta/application/views/Head.php
+++ LP01_1703027_RegySaputra/kereta/application/views/Head.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>PR6 PROMNET</title>
+  <title>Promnet</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="<?php echo base_url(); ?>/css/bootstrap.min.css">
